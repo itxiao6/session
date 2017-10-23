6374dfbfdb9731f4d886a554ca9cdcd406bd0238
戒尺 <itxiao6@qq.com>
2017-10-23 14:33:10 +0800

    本地存储器 Redis 存储器 密码验证

--- src/Storage/Local.php
+++ src/Storage/Local.php
@@ -8,5 +8,13 @@ namespace Itxiao6\Session\Storage;
  */
 class Local implements Storage
 {
-
+    /**
+     * 本地存储器
+     * @param string $session_dir
+     */
+    public function __construct($session_dir = '/tmp')
+    {
+        # 修改session文件的储存位置
+        session_save_path($session_dir);
+    }
 }
--- src/Storage/Redis.php
+++ src/Storage/Redis.php
@@ -10,13 +10,13 @@ class Redis implements Storage
 {
     /**
      * Redis 存储器
-     * Redis constructor.
      * @param string $host
      * @param int $port
+     * @param null $pwd
      */
-    public function __construct($host='127.0.0.1',$port = 6379)
+    public function __construct($host='127.0.0.1',$port = 6379,$pwd = null)
     {
         ini_set("session.save_handler", "redis");
-        ini_set("session.save_path", "tcp://{$host}:{$port}");
+        ini_set("session.save_path", "tcp://{$host}:{$port}".$pwd!=null?"?auth={$pwd}":'');
     }
 }
