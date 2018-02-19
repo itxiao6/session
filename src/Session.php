8309a4109d0d5b3d2eb0885dd793aa9566231d6a
mi pro <itxiao6@qq.com>
2018-02-19 23:55:51 +0800

    主入口重写，# TODO 重写存储驱动

--- src/Session.php
+++ src/Session.php
@@ -8,210 +8,243 @@ namespace Itxiao6\Session;
 class Session
 {
     /**
-     * 存储接口
+     * Session实例
      * @var array
      */
-    protected $interfaces = [
-        'Local'=>\Itxiao6\Session\Storage\Local::class,
-        'Redis'=>\Itxiao6\Session\Storage\Redis::class,
-        'Mysql'=>\Itxiao6\Session\Storage\Mysql::class
+    protected $example = [];
+    /**
+     * session 构造器配置
+     * @var null|array
+     */
+    protected $session_config = [
+        'driver'=>'Local',
+        'session_name'=>'PHPSESSION',
+        'session_path'=>'/',
+        // 默认一周的有效期
+        'session_expire'=>0,
+        'interfaces'=>[
+            'Local'=>\Itxiao6\Session\Storage\Local::class,
+            'Redis'=>\Itxiao6\Session\Storage\Redis::class,
+            'Mysql'=>\Itxiao6\Session\Storage\Mysql::class
+        ],
     ];
     /**
-     * 存储驱动实例
-     * @var null | object
+     * 是否已经启动session
+     * @var bool
      */
-    protected $storage = null;
+    protected $is_start = false;
     /**
-     * 使用驱动
-     * @var bool | string
-     */
-    protected $driver = 'Local';
-    /**
-     * Session实例
-     * @var null | object
-     */
-    protected $example = null;
-    /**
-     * Session 名称
-     * @var string
-     */
-    protected $name = 'Minkernel';
-    /**
-     * 请求实例 (swoole 模式下才用的到)
-     * @var null | object
+     * 请求
+     * @var null | Request
      */
     protected $request = null;
     /**
-     * 响应实例 (swoole 模式下才用的到)
-     * @var null | object
+     * 响应
+     * @var null | Response
      */
     protected $response = null;
 
     /**
      * 构造方法
      */
-    protected function __construct()
+    protected function __construct($request,$response)
     {
-
+        # 设置请求
+        $this -> request = $request;
+        # 设置响应
+        $this -> response = $response;
+        # 判断是否已经设置了 session 有效期
+        if($this -> config('session_expire') < 1){
+            $this -> config('session_expire',604800);
+        }
+        # 获取请求内的session id
+        $session_id = isset($request -> RawRequest() -> cookie[$this -> config('session_name')])?$request -> RawRequest() -> cookie[$this -> config('session_name')]:false;
+        # 判断是否需要重置 session id
+        if(in_array($session_id,[null,false,''])){
+            $this -> session_id(self::getARandLetter(30));
+        }else{
+            $this -> session_id($session_id);
+        }
     }
 
     /**
      * 获取接口
+     * @param $request
+     * @param $response
+     * @return static
      */
-    public static function getInterface()
+    public static function getInterface($request,$response)
     {
-        return new static(...func_get_args());
+        return new static($request,$response);
     }
 
     /**
-     * 启动session 回话
-     * @return $this
+     * 启动会话
      */
-    public function session_start()
+    public function start()
     {
+        /**
+         * 判断session id 是否存在
+         */
+        if($this -> config('session_id') === null){
+            $this -> reset_session_id();
+        }
         /**
          * 实例化存储器
          */
-        $this -> storage = new $this -> interfaces[$this -> driver](...func_get_args());
+        if($this -> is_start){
+            throw new \Exception('会话已经启动');
+        }
+        /**
+         * 判断有效期是否小于当前时间
+         */
+        if($this -> expire() < time()){
+            $this -> expire($this -> expire());
+        }
+        $class = $this -> config('interfaces')[$this -> config('driver')];
+        $this -> example = new $class($this);
+        # 设置session_id 到cookie
+        $this -> response -> RawResponse() -> cookie($this -> session_name(),$this -> session_id(),$this -> expire(),$this -> path());
         return $this;
     }
 
     /**
-     * 设置使用的驱动
-     * @param $type
+     * 重置session_id
      */
-    public function set_driver($type)
+    public function reset_session_id()
     {
-        $this -> driver = $type;
+        # 获取session id 并且 更改配置
+        $this -> session_id(self::getARandLetter(30));
     }
 
     /**
-     * 获取驱动
+     * cookie 路径设置或 获取
+     * @param null|string $val
+     * @return Session|mixed
+     */
+    public function path($val = null)
+    {
+        if($val === null){
+            return $this -> config('session_path');
+        }else{
+            return $this -> config('session_path',$val);
+        }
+    }
+    /**
+     * 设置或获取 过期时间
+     * @param null|int $time
+     * @return Session|mixed
+     */
+    public function expire($time = null)
+    {
+        if($time === null){
+            return $this -> config('session_expire');
+        }else{
+            return $this -> config('session_expire',time()+$time);
+        }
+    }
+    /**
+     * 获取随机字符串
+     * @param int $number
      * @return bool|string
      */
-    public function get_driver()
+    protected static function getARandLetter($number = 1)
     {
-        return $this -> driver;
-    }
-
-    /**
-     * 获取存储接口
-     * @param null $key
-     * @return array|mixed
-     */
-    public function get_interface($key = null)
-    {
-        if(self::$key!=null){
-            return $this -> interfaces[$key];
-        }else{
-            return $this -> interfaces;
+        # 判断长度是否为0
+        if ($number == 0){return false;}
+        # 如果小于零取正值
+        $number = $number < 0 ? - $number : $number;
+        $letterArr = array ('A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' );
+        $returnStr ='';
+        for($i= 0; $i < $number; $i ++) {
+            $returnStr .= $letterArr [rand ( 0, 51 )];
         }
+        return $returnStr;
     }
 
     /**
-     * 设置存储接口
-     * @param null|array $key
-     * @param null|string $value
-     * @return $this
+     * 设置配置
+     * @param null|string $name
+     * @param null|mixed $value
+     * @return $this|mixed
      */
-    public function set_interface($key = null,$value = null)
+    protected function config($name = null,$value=null)
     {
-        if(is_array($key) && $value==null){
-            foreach ($key as $key=>$val){
-                $this -> interfaces[$key] = $val;
+        if($name === null && $value === null){
+            return $this -> session_config;
+        }
+        if(is_string($name) && $value === null){
+            return isset($this -> session_config[$name])?$this -> session_config[$name]:null;
+        }
+        if(is_array($name) && count($name) > 0 && $value === null){
+            foreach ($name as $key=>$item){
+                $this -> config($key,$item);
             }
         }else{
-            $this -> interfaces[$key] = $value;
+            $this -> session_config[$name] = $value;
         }
         return $this;
     }
 
     /**
-     * 设置存储实例
-     * @param $object
+     * 使用的驱动
+     * @param $driverName
+     * @return Session
+     */
+    public function driver($driverName,$driverClass = null)
+    {
+        if($driverClass === null){
+            return $this -> config('driver');
+        }else{
+            return $this -> config('driver',$driverName);
+        }
+    }
+
+    /**
+     * 存储接口
+     * @param $key
+     * @param null $value
      * @return $this
      */
-    public function set_storage($object)
+    public function interface($key = null,$value = null)
     {
-        $this -> storage = $object;
-        return $this;
-    }
-
-    /**
-     * 获取存储实例
-     * @return null|object
-     */
-    public function get_storage()
-    {
-        return $this -> storage;
-    }
-
-    /**
-     * 获取session 操作实例
-     * @return null|object
-     */
-    public function get_example()
-    {
-        return $this -> example;
+        if(is_string($key) && $value === null){
+            return $this -> config('interfaces')[$key];
+        }else if($key === null && $value === null){
+            return $this -> config('interfaces');
+        }
+        if(is_array($key) && count($key) > 0 && $value==null){
+            foreach ($key as $k=>$val){
+                $this -> config('interfaces',array_merge($this -> config('interfaces'),[$k=>$val]));
+            }
+        }else{
+            return $this -> config('interfaces',array_merge($this -> config('interfaces'),[$key=>$value]));
+        }
     }
 
     /**
      * 设置session 名称
-     * @param $name
-     * @return $this
+     * @param null $name
+     * @return $this|Session|mixed
      */
-    public function set_session_name($name)
+    public function session_name($name = null)
     {
-        $this -> name = $name;
-        return $this;
+        if($name === null){
+            return $this -> config('session_name');
+        }
+        return $this -> config('session_name',$name);
     }
 
     /**
-     * 获取session 名称
-     * @return string
+     * 设置session id
+     * @param $id
+     * @return $this|Session|mixed
      */
-    public function get_session_name()
+    public function session_id($id = null)
     {
-        return $this -> name;
-    }
-
-    /**
-     * 设置请求 (swoole 模式下才用的到)
-     * @param $request
-     * @return $this
-     */
-    public function set_request($request)
-    {
-        $this -> request = $request;
-        return $this;
-    }
-
-    /**
-     * 获取请求 (swoole 模式下才用的到)
-     * @return null|object
-     */
-    public function get_request()
-    {
-        return $this -> request;
-    }
-
-    /**
-     * 设置响应 (swoole 模式下才用的到)
-     * @param $response
-     * @return $this
-     */
-    public function set_response($response)
-    {
-        $this -> response = $response;
-        return $this;
-    }
-
-    /**
-     * 获取响应 (swoole 模式下才用的到)
-     * @return null|object
-     */
-    public function get_response()
-    {
-        return $this -> response;
+        if($id === null){
+            return $this -> config('session_id');
+        }
+        return $this -> config('session_id',$id);
     }
 }
--- src/Tools/Cookie.php
+++ src/Tools/Cookie.php
@@ -10,12 +10,44 @@ use \Itxiao6\Session\Session;
 class Cookie
 {
     /**
+     * 请求
+     * @var
+     */
+    protected $request;
+    /**
+     * 响应
+     * @var
+     */
+    protected $response;
+    /**
+     * 获取cookie 操作实例
+     * @param Request $request
+     * @param Response $response
+     */
+    public static function getInterface(Request $request,Response $response)
+    {
+        return new static($request,$response);
+    }
+
+    /**
+     * 构造方法
+     * Cookie constructor.
+     * @param Request $request
+     * @param Response $response
+     */
+    protected function __construct(Request $request,Response $response)
+    {
+        $this -> request = $request;
+        $this -> response = $response;
+    }
+
+    /**
      * 设置cookie
      */
-    public static function set_cookie()
+    public function set_cookie()
     {
         if(PHP_SAPI === 'cli'){
-            return Session::get_response() -> cookie(...func_get_args());
+            # TODO swoole 设置cookie
         }else{
             return setcookie(...func_get_args());
         }
@@ -26,10 +58,10 @@ class Cookie
      * @param null $name
      * @return mixed
      */
-    public static function get_cookie($name = null)
+    public function get_cookie($name = null)
     {
         if(PHP_SAPI === 'cli'){
-            return ($name===null)?Session::get_request() -> cookie:(isset(Session::get_request() -> cookie[$name])?Session::get_request() -> cookie[$name]:null);
+            # TODO swoole 获取cookie
         }else{
             return ($name===null)?$_COOKIE:(isset($_COOKIE[$name])?$_COOKIE[$name]:null);
         }
